4badd5f454996685939685698d0b362b6a255b9e
vyshiva <elena_haponyuk@ukr.net>
2018-04-18 23:56:00 +0300

    Implementing module pattern

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
+// BUDGET CONTROLLER
 var budgetController = (function(){
-  var x = 23;
 
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b){
-      return add(b);
-    }
-  }
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
 })();
 
+// GLOBAL APP CONTROLLER
 var Controller = (function(budgetCtrl, UICtrl) {
-
-  var z = budgetCtrl.publicTest(5);
-
-  return {
-    anotherPublic: function () {
-      console.log(z);
-    }
-  }
 
 })(budgetController, UIController);
