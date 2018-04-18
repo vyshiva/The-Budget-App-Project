31e26295361dea45c9e8e127571ad3c10a63ff4b
vyshiva <elena_haponyuk@ukr.net>
2018-04-18 14:05:50 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var budgetController = (function(){
+  var x = 23;
+
+  var add = function(a) {
+    return x + a;
+  }
+
+  return {
+    publicTest: function(b){
+      return add(b);
+    }
+  }
+})();
+
+var UIController = (function() {
+
+})();
+
+var Controller = (function(budgetCtrl, UICtrl) {
+
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function () {
+      console.log(z);
+    }
+  }
+
+})(budgetController, UIController);
